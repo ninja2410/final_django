c943391c760e37697bf035982028d46ba1f971fe
ninja2410 <pablo.felg1996@gmail.com>
2018-11-06 00:47:57 -0600

    Inicio del proyecto, implementación de modelos

---
+++ equipos/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+
+from django.contrib import admin
+
+
+class Jugador(models.Model):
+
+    nombre  =   models.CharField(max_length=30)
+    procedencia  =   models.CharField(max_length=50)
+    dorsal  =   models.CharField(max_length=30)
+    fecha_nacimiento = models.DateField()
+
+
+    def __str__(self):
+
+        return self.nombre
+
+
+class Equipo(models.Model):
+    nombre    = models.CharField(max_length=60)
+    anio      = models.IntegerField()
+    jugadores   = models.ManyToManyField(Jugador, through='Partido')
+
+    def __str__(self):
+
+        return self.nombre
+
+
+class Partido (models.Model):
+    jugador = models.ForeignKey(Jugador, on_delete=models.CASCADE)
+    partido = models.ForeignKey(Partido, on_delete=models.CASCADE)
+
+
+class PartidoInLine(admin.TabularInline):
+    model = Partido
+    extra = 1
+
+
+class JugadorAdmin(admin.ModelAdmin):
+    inlines = (PartidoInLine,)
+
+
+class EquuipoAdmin (admin.ModelAdmin):
+
+    inlines = (PartidoInLine,)
