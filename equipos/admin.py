243775e559603212f14534ba47952dd53607a292
ninja2410 <pablo.felg1996@gmail.com>
2018-11-06 23:31:44 -0600

    Registro de urls y modelos en admin de proyecto

--- equipos/admin.py
+++ equipos/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from peliculas.models import Jugador, JugadorAdmin, Equipo, EquipoAdmin
 # Register your models here.
+admin.site.register(Jugador, JugadorAdmin)
+admin.site.register(Equipo, EquipoAdmin)
